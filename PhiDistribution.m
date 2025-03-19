40b82d918431d1f4b403d42ac65003f5c3c964cd
Reece-Pruneau <reece.pruneau@ised-isde.gc.ca>
2025-03-19 13:27:40 -0400

    Converted to Function
    Converted to function so it need not be run separately, and no longer uses global variables. All values are also parameterized.

--- PhiDistribution.m
+++ PhiDistribution.m
@@ -1,25 +1,24 @@
+function [HEXphi,HEXtheta,HEXd] = PhiDistribution(h_UE,h_BS,r_a,n_a)
 
-r_a=6000;
-n_a=1000000;
-UE_a=zeros(3,n_a);
-tilt=-3;
-h=40;
 
+UE_a=NaN(2,n_a);
 %generate n_a random xy positions in cell
 UE_a(1:2,:)=randHEX(r_a,0,0,n_a);
 
 %Altitude is zero
-UE_a(3,:)=0;
-global HEXphi
-global HEXtheta
-global HEXd
+UE_a(3,:)=h_UE;
+
+HEXphi=NaN(1,n_a);
+HEXtheta=NaN(1,n_a);
+HEXd=NaN(1,n_a);
+
 for i=1:n_a
     
- D=[UE_a(1,i),UE_a(2,i)-r_a,-h]; %displacment vector
-        HEXphi(i)=real(180.*acos(dot(D,[0,-1,sin(pi*tilt/180)])./norm(D))./pi);
-        HEXtheta(i)=90+tilt-180.*atan(norm(D)./h)./pi;
+ D=[UE_a(1,i),UE_a(2,i)-r_a,h_UE-h_BS]; %displacment vector
+        HEXphi(i)=acosd(dot(D(1:2),[0,-1])./norm(D(1:2)));
+        HEXtheta(i)=acosd(dot([norm(D(1:2)),D(3)],[1,0])./norm(D));      
         HEXd(i)=norm(D(1:2));
 end
 
 
-
+end
